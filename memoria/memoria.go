d0b7699f2fc15797c2b3c39da9548bfb0535eaaa
JazminAgostinaRuiz <jaruiz@frba.utn.edu.ar>
2024-05-01 15:25:29 +0000

    Se creo el  nuevo file utils para funciones de memoria y se agrego funcionalidad para recibir el path

--- memoria/memoria.go
+++ memoria/memoria.go
@@ -1,57 +1,18 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
-	"os"
 )
 
 //var memory = make(map[int]string)
 
 func main() {
-	readFile("../leer.txt") //con ../ busca el que esta en general, con ./ o sin aclarar busca el de memoria
+	//readFile("../leer.txt") //con ../ busca el que esta en general, con ./ o sin aclarar busca el de memoria
 	mux := http.NewServeMux()
-
-	mux.HandleFunc("GET /memoria", crearProceso)
-
+	//mux.HandleFunc("DELETE /process", finalizarProceso)
+	mux.HandleFunc("PUT /process", crearProceso)
 	err := http.ListenAndServe(":8002", mux)
 	if err != nil {
 		panic(err)
 	}
-}
-
-func readFile(fileName string) {
-	file, err := os.Open(fileName) // For read access.
-	if err != nil {
-		log.Fatal(err)
-	}
-	data := make([]byte, 100)
-	count, err := file.Read(data)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("read %d bytes: %q\n", count, data[:count])
-}
-
-/* func allocateMemory(w http.ResponseWriter, r *http.Request) {
-	address := len(memory)
-	memory[address] = "DATA"
-
-	fmt.Fprintf(w, "Memory allocated at address %d", address)
-}*/
-
-func crearProceso(w http.ResponseWriter, r *http.Request) {
-
-	respuesta, err := json.Marshal("se crea un nuevo proceso")
-	if err != nil {
-		http.Error(w, "Error al codificar los datos como JSON", http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write(respuesta)
-	log.Print("se creo proceso exitosamente")
-
 }
---
+++ memoria/utils.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func readFile(fileName string) {
+	file, err := os.Open(fileName) // For read access.
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("read %d bytes: %q\n", count, data[:count])
+}
+
+/*
+	 func allocateMemory(w http.ResponseWriter, r *http.Request) {
+		address := len(memory)
+		memory[address] = "DATA"
+
+		fmt.Fprintf(w, "Memory allocated at address %d", address)
+	}
+*/
+type BodyRequest struct {
+	Path string `json:"path"`
+}
+
+func crearProceso(w http.ResponseWriter, r *http.Request) {
+	var request BodyRequest
+
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	//respuesta, err := json.Marshal(response.PID)
+	if err != nil {
+		http.Error(w, "Error al codificar los datos como JSON", http.StatusInternalServerError)
+		return
+	}
+	readFile(request.Path)
+	respuesta, err := json.Marshal("se crea un nuevo proceso")
+	if err != nil {
+		http.Error(w, "Error al codificar los datos como JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(respuesta)
+	log.Print("se creo proceso exitosamente")
+
+}
