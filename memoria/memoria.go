bcd75c30aa320cd8608a60bc9caa4320c918dec3
Ian Sanna <ian.sanna@outlook.com>
2024-05-02 21:16:34 +0000

    Creacion y utilizacion del config

--- memoria/memoria.go
+++ memoria/memoria.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"memoria/globals"
+	"memoria/utils"
 	"net/http"
+	"strconv"
 )
 
 //var memory = make(map[int]string)
 
 func main() {
-	//readFile("../leer.txt") //con ../ busca el que esta en general, con ./ o sin aclarar busca el de memoria
+	utils.ConfigurarLogger()
+
+	globals.ClientConfig = utils.IniciarConfiguracion("config.json")
+
 	mux := http.NewServeMux()
+
 	//mux.HandleFunc("DELETE /process", finalizarProceso)
-	mux.HandleFunc("PUT /process", crearProceso)
-	err := http.ListenAndServe(":8002", mux)
+	mux.HandleFunc("PUT /process", utils.CrearProceso)
+
+	err := http.ListenAndServe(":"+strconv.Itoa(globals.ClientConfig.Port), mux)
 	if err != nil {
 		panic(err)
 	}
--- memoria/utils/utils.go
+++ memoria/utils/utils.go
@@ -1,12 +1,42 @@
-package main
+package utils
 
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"memoria/globals"
 	"net/http"
 	"os"
 )
+
+type BodyRequest struct {
+	Path string `json:"path"`
+}
+
+func IniciarConfiguracion(filePath string) *globals.Config {
+	var config *globals.Config
+
+	configFile, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer configFile.Close()
+
+	jsonParser := json.NewDecoder(configFile)
+	jsonParser.Decode(&config)
+
+	return config
+}
+
+func ConfigurarLogger() {
+	logFile, err := os.OpenFile("logs/memoria.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		panic(err)
+	}
+	mw := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(mw)
+}
 
 func readFile(fileName string) {
 	file, err := os.Open(fileName) // For read access.
@@ -29,11 +59,8 @@ func readFile(fileName string) {
 		fmt.Fprintf(w, "Memory allocated at address %d", address)
 	}
 */
-type BodyRequest struct {
-	Path string `json:"path"`
-}
 
-func crearProceso(w http.ResponseWriter, r *http.Request) {
+func CrearProceso(w http.ResponseWriter, r *http.Request) {
 	var request BodyRequest
 
 	err := json.NewDecoder(r.Body).Decode(&request)
@@ -41,7 +68,9 @@ func crearProceso(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
 	readFile(request.Path)
+
 	respuesta, err := json.Marshal("se crea un nuevo proceso")
 	if err != nil {
 		http.Error(w, "Error al codificar los datos como JSON", http.StatusInternalServerError)
